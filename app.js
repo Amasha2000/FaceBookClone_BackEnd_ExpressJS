ce1d8e479d9547e4328f3861ce06cd15f1e31cb5
Amasha2000 <amashagalhenage@gmail.com>
2022-08-27 11:24:30 +0530

    Created database and connection

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const port = 4000;
+
+const url = 'mongodb://127.0.0.1/facebook_clone'
+
+const app = express()
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const con = mongoose.connection
+
+con.on('open', () => {
+  console.log('Connected')
+})
+
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log('Server Started')
+})
+
+
+
+
